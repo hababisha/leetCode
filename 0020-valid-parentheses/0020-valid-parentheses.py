1538ecc8a80a9ac3f0a558843e80dee3072e102c
Haileamlak <haileamlakderbie1@gmail.com>
2025-01-15 21:50:28 +0300

    Time: 3 ms (24.07%), Space: 17.8 MB (38.99%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stk = []
+        m = {")":"(",
+             "]":"[",
+             "}":"{"}
+        for c in s:
+            if c in m:
+                if stk and stk[-1]==m[c]:
+                    stk.pop()
+                else:
+                    return False
+            else:
+                stk.append(c)
+
+        return True if not stk else False
+        
+
+        
+            
+                    
+        
