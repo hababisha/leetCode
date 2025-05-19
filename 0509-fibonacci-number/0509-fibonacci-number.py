1754ef3c2f8368e8f3ccb2784f673355383ef573
Haileamlak <haileamlakderbie1@gmail.com>
2025-05-19 11:46:30 +0300

    Time: 1625 ms (5.17%), Space: 17.7 MB (41.38%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,6 +1,11 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n < 2:
-            return n
-        else:
-            return self.fib(n-1) + self.fib(n-2)
+        def dp(n):
+            if n < 2: return n
+            if n in cache: return cache[n]
+
+            cache[n] = self.fib(n-2) + self.fib(n-1)
+            return cache[n]
+        
+        cache = {}
+        return dp(n)
