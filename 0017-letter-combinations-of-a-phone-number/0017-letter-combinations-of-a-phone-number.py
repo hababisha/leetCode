fe3fdd0ae81086a04dbe8d9cbe346d6eae2a6004
Haileamlak <haileamlakderbie1@gmail.com>
2025-06-24 14:39:07 +0300

    Time: 0 ms (100%), Space: 17.8 MB (68.02%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -3,13 +3,23 @@ class Solution:
         if not digits:
             return []
       
-        digit_to_chars = ['abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
-      
-        combinations = ['']
-      
-        for digit in digits:
-            letters = digit_to_chars[int(digit) - 2]
-          
-            combinations = [prefix + letter for prefix in combinations for letter in letters]
-      
-        return combinations
+        digit_to_chars = {  "2":'abc',
+                            "3": 'def',
+                            "4":'ghi',
+                            "5":'jkl',
+                            "6": 'mno', 
+                            "7":'pqrs', 
+                            "8":'tuv', 
+                            "9":'wxyz'}
+        res = []
+        
+
+        def backtrack(i, curStr):
+            if len(curStr) == len(digits):
+                res.append(curStr)
+                return
+            for c in digit_to_chars[digits[i]]:
+                backtrack(i+1, curStr +c)
+
+        backtrack(0, "")
+        return res
