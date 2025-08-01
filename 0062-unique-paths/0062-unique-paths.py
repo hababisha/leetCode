bcdf7e0a28938dfb95e44209bb54540bece5c23f
Haileamlak <haileamlakderbie1@gmail.com>
2025-08-01 21:31:22 +0300

    Time: 3 ms (21.55%), Space: 18.2 MB (5.57%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,14 +1,16 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        def dfs(r,c):
-            if r < 0 or r >= m or c < 0 or c >= n:
-                return 0
-            if r == m-1 and c == n-1:
-                return 1
-            count = 0
-            count += dfs(r+1,c)
-            count += dfs(r, c+1)
+        memo = {}
 
-            return count
-        
-        return dfs(0,0)
+        def dp(i, j):
+            if i == m or j == n:
+                return 0
+            if (i, j) == (m - 1, n - 1):
+                return 1
+
+            if (i, j) not in memo:
+                memo[(i, j)] = dp(i + 1, j) + dp(i, j + 1)
+
+            return memo[(i, j)]
+
+        return dp(0, 0)
