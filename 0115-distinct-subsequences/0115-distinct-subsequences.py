f7331cfbffbe31c4b7a66a47d24427dbb256abe1
Haileamlak <haileamlakderbie1@gmail.com>
2025-08-08 21:50:54 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        memo = {}
+
+        def dp(i,j):
+            if j == len(t):
+                return 1
+            if i == len(s):
+                return 0
+            if (i,j) not in memo:
+                if s[i] == t[j]:
+                    memo[(i,j)]= dp(i+1,j+1) + dp(i+1, j)
+                else:
+                    memo[(i,j)] = dp(i+i, j)
+            return memo[(i,j)]
+        
+        return dp(0,0)
