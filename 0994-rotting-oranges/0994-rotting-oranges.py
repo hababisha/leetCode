3d2bba27f2502580d908cb31fb5cafafaf6d367f
Haileamlak <haileamlakderbie1@gmail.com>
2025-10-13 00:35:55 +0300

    Time: 5 ms (33.46%), Space: 17.7 MB (93.2%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,29 +1,36 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        fresh, time = 0, 0
-
         q = deque()
         m, n = len(grid), len(grid[0])
+        dirs = [[0,1], [1,0], [-1, 0], [0,-1]]
+        seen = set()
+        fresh, time = 0, 0
+
+        def inbound(x, y):
+            return 0 <= x < m and 0 <= y < n
 
         for r in range(m):
             for c in range(n):
+                if grid[r][c] == 2:
+                    q.append((r,c))
+                    seen.add((r,c))
+                
                 if grid[r][c] == 1:
                     fresh += 1
-                if grid[r][c] == 2:
-                    q.append([r,c])
-        directions = [[0,1],[0,-1], [-1,0],[1,0]]
+        
         while q and fresh > 0:
             for _ in range(len(q)):
                 x, y = q.popleft()
+                seen.add((x,y))
 
-                for dx, dy in directions:
+                for dx, dy in dirs:
                     nx, ny = x + dx, y + dy
-                
-                    if nx < 0 or nx == m or ny < 0 or ny == n or grid[nx][ny] != 1:
+                    if (nx, ny) in seen or not inbound(nx,ny) or grid[nx][ny] != 1:
                         continue
-                
-                    grid[nx][ny] = 2
-                    q.append([nx,ny])
+                    
+                    seen.add((nx,ny))
+                    q.append((nx,ny))
                     fresh -= 1
+                    grid[nx][ny] = 2 
             time += 1
         return time if fresh == 0 else -1
