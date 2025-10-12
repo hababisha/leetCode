653b212c7a98263daacad052e713d6236e89cce2
Haileamlak <haileamlakderbie1@gmail.com>
2025-10-13 00:33:40 +0300

    Time: 6 ms (28.48%), Space: 17.7 MB (71.12%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,30 +1,29 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        fresh, time = 0,0
-        q = deque()
-        ROWS, COLS = len(grid), len(grid[0])
+        fresh, time = 0, 0
 
-        for r in range(ROWS):
-            for c in range(COLS):
+        q = deque()
+        m, n = len(grid), len(grid[0])
+
+        for r in range(m):
+            for c in range(n):
                 if grid[r][c] == 1:
                     fresh += 1
                 if grid[r][c] == 2:
                     q.append([r,c])
-
-        directions = [[0,1],[0,-1],[1,0],[-1,0]]
+        directions = [[0,1],[0,-1], [-1,0],[1,0]]
         while q and fresh > 0:
-            for i in range(len(q)):
-                r,c = q.popleft()
-                for dr, dc in directions:
-                    row = r + dr
-                    col = c + dc
+            for _ in range(len(q)):
+                x, y = q.popleft()
 
-                    if (row < 0 or row == len(grid) or
-                        col < 0 or col == len(grid[0]) or
-                        grid[row][col] != 1):
+                for dx, dy in directions:
+                    nx, ny = x + dx, y + dy
+                
+                    if nx < 0 or nx == m or ny < 0 or ny == n or grid[nx][ny] != 1:
                         continue
-                    grid[row][col] = 2
-                    q.append([row,col])
+                
+                    grid[nx][ny] = 2
+                    q.append([nx,ny])
                     fresh -= 1
             time += 1
         return time if fresh == 0 else -1
