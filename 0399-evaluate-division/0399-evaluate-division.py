9c5bc81106d6c78b0036848feebca184c51e961e
Haileamlak <haileamlakderbie1@gmail.com>
2025-11-28 10:17:37 +0300

    Time: 0 ms (100%), Space: 17.7 MB (93.58%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,25 +1,38 @@
 class Solution:
-    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+    def calcEquation(self, equations, values, queries):
+        from collections import defaultdict, deque
+        
         graph = defaultdict(list)
 
-        for i, eq in enumerate(equations):
-            a, b = eq
-            graph[a].append((b, values[i]))
-            graph[b].append((a, 1/values[i]))
+        for (a, b), val in zip(equations, values):
+            graph[a].append((b, val))
+            graph[b].append((a, 1 / val))
         
-        def bfs(i,j):
-            if i not in graph or j not in graph:
-                return -1
-            q, visit= deque(), set()
-            q.append([i, 1])
-            visit.add(i)
-            while q:
-                node, product = q.popleft()
-                if node == j:
-                    return product
-                for nei,weight in graph[node]:
-                    if nei not in visit:
-                        q.append([nei, product*weight])
-                        visit.add(nei)
-            return -1
-        return [bfs(q[0], q[1]) for q in queries]            
+        def solve(src, dst):
+            if src not in graph or dst not in graph:
+                return -1.0
+            if src == dst:
+                return 1.0
+
+            stack = [(src, 1.0)]
+            visited = set()
+
+            while stack:
+                node, cur_val = stack.pop()
+
+                if node == dst:
+                    return cur_val
+
+                visited.add(node)
+
+                for nei, weight in graph[node]:
+                    if nei not in visited:
+                        stack.append((nei, cur_val * weight))
+
+            return -1.0
+        
+        ans = []
+        for num, den in queries:
+            ans.append(solve(num, den))
+
+        return ans
