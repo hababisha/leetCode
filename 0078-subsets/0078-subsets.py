18e9e5e4e10a0bcd26b0e1ed5958652827dfd15f
Haileamlak <haileamlakderbie1@gmail.com>
2025-12-24 13:38:58 +0300

    Time: 0 ms (100%), Space: 18.1 MB (19.39%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,20 +1,17 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        sol = []
-        n = len(nums)
+        res, sol = [], []
 
         def dfs(i):
-            if i == n:
+            if i == len(nums):
                 res.append(sol[:])
                 return
             
-            dfs(i+1)
 
             sol.append(nums[i])
             dfs(i+1)
             sol.pop()
 
-
+            dfs(i+1)
         dfs(0)
         return res
