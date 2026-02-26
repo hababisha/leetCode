fad33eeca25cdcffb2f8fa177946e921859c9ca2
Haileamlak <haileamlakderbie1@gmail.com>
2026-02-26 19:52:31 +0300

    Time: 4 ms (25.49%), Space: 19.4 MB (71.37%) - LeetHub

---
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -0,0 +1,5 @@
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        m = {v:i for i, v in enumerate(arr2)}
+
+        return sorted(arr1, key=lambda x: (m.get(x, float("inf")),x))
